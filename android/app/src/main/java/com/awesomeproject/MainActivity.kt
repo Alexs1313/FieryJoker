c1b7e38cf29cf7258811e08f0ca316ed421d0d99
Alexs1313 <aleshaalesha1313@gmail.com>
2025-04-21 15:32:24 +0300

    update

--- android/app/src/main/java/com/awesomeproject/MainActivity.kt
+++ android/app/src/main/java/com/awesomeproject/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.awesomeproject
 import android.os.Bundle;
+import android.content.Intent;
+import android.content.res.Configuration;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
